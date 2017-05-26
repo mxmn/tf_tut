a9d9a1ab286c1696dbc0794f9ba5f66f49bd9e20
Maxim Neumann <neumann.maxim@gmail.com>
2017-05-25 20:41:11 -0700

    Commiting old changes for tf 0.x

---
+++ scripts/tut8_tf_rnn_seqs.py
@@ -0,0 +1,134 @@
+import numpy as np
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import time
+import os
+import urllib.request
+from tensorflow.models.rnn.ptb import reader
+
+dpath = os.environ['HOME'] + '/data/tf_tut/tut8/'
+
+file_url = 'https://raw.githubusercontent.com/jcjohnson/torch-rnn/master/data/tiny-shakespeare.txt'
+file_name = os.environ['HOME'] + '/data/tf_tut/tinyshakespeare.txt'
+if not os.path.exists(file_name):
+    urllib.request.urlretrieve(file_url, file_name)
+
+with open(file_name,'r') as f:
+    raw_data = f.read()
+    print("Data length:", len(raw_data))
+
+vocab = set(raw_data)
+vocab_size = len(vocab)
+idx_to_vocab = dict(enumerate(vocab))
+vocab_to_idx = dict(zip(idx_to_vocab.values(), idx_to_vocab.keys()))
+
+data = [vocab_to_idx[c] for c in raw_data]
+del raw_data
+
+print(vocab_size)
+print(min(data), max(data))
+print(data[:100])
+
+
+
+
+def gen_epochs(n, num_steps, batch_size):
+    for i in range(n):
+        yield reader.ptb_iterator(data, batch_size, num_steps)
+
+def reset_graph():
+    if 'sess' in globals() and sess:
+        sess.close()
+    tf.reset_default_graph()
+
+def train_network(g, num_epochs, num_steps = 200, batch_size = 32, verbose = True, save=False):
+    tf.set_random_seed(2345)
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+        sess.run(tf.initialize_all_variables())
+        training_losses = []
+        for idx, epoch in enumerate(gen_epochs(num_epochs, num_steps, batch_size)):
+            training_loss = 0
+            steps = 0
+            training_state = None
+            for X, Y in epoch:
+                steps += 1
+
+                feed_dict={g['x']: X, g['y']: Y}
+                if training_state is not None:
+                    feed_dict[g['init_state']] = training_state
+                training_loss_, training_state, _ = sess.run([g['total_loss'],
+                                                      g['final_state'],
+                                                      g['train_step']],
+                                                             feed_dict)
+                training_loss += training_loss_
+            if verbose:
+                print("{}: Average training loss for Epoch {}: {}".format(
+                        time.ctime(), idx, training_loss/steps))
+            training_losses.append(training_loss/steps)
+
+        if isinstance(save, str):
+            g['saver'].save(sess, save)
+
+    return training_losses
+
+
+
+
+
+
+def build_multilayer_lstm_graph_with_dynamic_rnn(
+    state_size = 100,
+    num_classes = vocab_size,
+    batch_size = 32,
+    num_steps = 200,
+    num_layers = 3,
+    learning_rate = 1e-4):
+
+    reset_graph()
+
+    x = tf.placeholder(tf.int32, [batch_size, num_steps], name='input_placeholder')
+    y = tf.placeholder(tf.int32, [batch_size, num_steps], name='labels_placeholder')
+
+    embeddings = tf.get_variable('embedding_matrix', [num_classes, state_size])
+
+    # Note that our inputs are no longer a list, but a tensor of dims batch_size x num_steps x state_size
+    rnn_inputs = tf.nn.embedding_lookup(embeddings, x)
+
+    cell = tf.nn.rnn_cell.LSTMCell(state_size, state_is_tuple=True)
+    cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers, state_is_tuple=True)
+    init_state = cell.zero_state(batch_size, tf.float32)
+    rnn_outputs, final_state = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=init_state)
+
+    with tf.variable_scope('softmax'):
+        W = tf.get_variable('W', [state_size, num_classes])
+        b = tf.get_variable('b', [num_classes], initializer=tf.constant_initializer(0.0))
+
+    #reshape rnn_outputs and y so we can get the logits in a single matmul
+    rnn_outputs = tf.reshape(rnn_outputs, [-1, state_size])
+    y_reshaped = tf.reshape(y, [-1])
+
+    logits = tf.matmul(rnn_outputs, W) + b
+
+    total_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, y_reshaped))
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(total_loss)
+
+    return dict(
+        x = x,
+        y = y,
+        init_state = init_state,
+        final_state = final_state,
+        total_loss = total_loss,
+        train_step = train_step
+    )
+
+
+t = time.time()
+build_multilayer_lstm_graph_with_dynamic_rnn()
+print("It took", time.time() - t, "seconds to build the graph.")
+
+
+g = build_multilayer_lstm_graph_with_dynamic_rnn()
+t = time.time()
+train_network(g, 3)
+print("It took", time.time() - t, "seconds to train for 3 epochs.")
